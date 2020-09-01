d3f80ada35d3c97bc8de6e151620a2e478ef405e
Edward Brunson <eddiebrunson@gmail.com>
2020-09-01 15:53:28 -0400

    Add configured upload_docker.sh:

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=webdeveddie/machinelproject4
 
 # Step 2:  
 # Authenticate & tag
+docker login --username=webdeveddie
+docker tag 9a2b3da60458 webdeveddie/machinelproject4:v.01
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push webdeveddie/machinelproject4
