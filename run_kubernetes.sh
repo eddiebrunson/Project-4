a784a0ad1121ad8156784e5134d2412f31defb72
Edward Brunson <eddiebrunson@gmail.com>
2020-09-01 16:39:48 -0400

    Update run_kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=webdeveddie/machinelproject4:v.01
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment machinelproject4-first --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods --all-namespaces
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/machinelproject4-first 800:80
+
+
 
